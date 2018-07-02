4448d4fe575cc10fed5afc8e65ba950cda9ed8e2
Pankaj <pankaj@webmavens.com>
2018-07-02 16:06:54 +0530

    serverstart file

--- serverstats.php
+++ serverstats.php
@@ -228,7 +228,6 @@
                                 <ul class="list-unstyled">
                                     <li>
                                         <div class="checkbox">
-                                        
                                         <?php      
                                             $sql = 'SELECT DISTINCT web_server_instance_name FROM ce_callobject_stats';
                                             mysql_select_db('cateyedashboard_db');
@@ -250,7 +249,6 @@
                                 <ul class="list-unstyled">
                                     <li>
                                         <div class="checkbox">
-                                        
                                         <?php      
                                             $sql = 'SELECT DISTINCT enterprise_server FROM ce_callobject_stats';
                                             mysql_select_db('cateyedashboard_db');
